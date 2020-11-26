de19e317b29ecc3ee5cb3a267dabcce2aeacd34c
Cameron Corrado <camcorrado@gmail.com>
2020-11-26 03:40:16 -0500

    Day 2 completed and adjustments to global syntax

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Day1 from "./Components/Day1/Day1";
+import Day2 from "./Components/Day2/Day2";
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <main className="App">
         <h1>Daily UI Challenges</h1>
         <Day1 />
+        <Day2 />
       </main>
     );
   }
